0334adb3b82098ca9b4be46e3aa273a6dfafebe5
hqmq <michael@riesd.com>
2014-02-26 15:35:51 -0700

    more thorough test

--- test_uniqueness.rb
+++ test_uniqueness.rb
@@ -1,22 +1,40 @@
 require_relative 'bootstrap'
 
+def many_workers
+  (1..10).map do |i|
+    Thread.new do
+      10.times do
+        Incrementer.perform_async
+      end
+    end
+  end.join
+end
+
+def print_counter
+  Sidekiq.redis do |conn|
+    puts "counter = #{conn.get :counter}"
+  end
+end
+
 Sidekiq.redis do |conn|
   conn.set :counter, 0
 end
 
-(1..10).map do |i|
-  Thread.new do
-    10.times do
-      Incrementer.perform_async
-    end
-  end
-end.join
+many_workers
+sleep 0.5 #let workers finish
+puts "Expecting counter = 1"
+print_counter
 
-sleep 1
+sleep 3 #less than 10 second timeout on class
 
-puts "Expecting the counter to = 1"
-Sidekiq.redis do |conn|
-  counter = conn.get :counter
-  puts "counter = #{counter}"
-end
+many_workers
+sleep 0.5 #let workers finish
+puts "Expecting counter = 1"
+print_counter
 
+sleep 10 #let the uniqueness timeout occur
+
+many_workers
+sleep 0.5
+puts "Expecting counter = 2"
+print_counter
--- workers/incrementer.rb
+++ workers/incrementer.rb
@@ -1,6 +1,6 @@
 class Incrementer
   include Sidekiq::Worker
-  sidekiq_options :unique => true
+  sidekiq_options :unique => true, :unique_job_expiration => 10
 
   def perform
     Sidekiq.redis do |conn|
